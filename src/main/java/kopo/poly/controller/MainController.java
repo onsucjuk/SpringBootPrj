6d32114987157ed1058580d74f1272fc91d8f056
JeongukChoi <onsucjuk@gmail.com>
2023-07-04 12:25:05 +0900

    Initial commit

---
+++ src/main/java/kopo/poly/controller/MainController.java
@@ -0,0 +1,22 @@
+package kopo.poly.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String main() throws Exception {
+        log.info(this.getClass().getName() + ".main 페이지 보여주는 함수 실행");
+        return "/main";
+    }
+
+    @GetMapping("/test")
+    public String test() throws Exception {
+        log.info(this.getClass().getName() + ".test 페이지 보여주는 함수 실행");
+        return "/test";
+    }
+}
